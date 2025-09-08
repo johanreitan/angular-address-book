5c49779bd3e9a3e2414f618fcf73e6c43533b662
Johan Reitan <johan.s.reitan@gmail.com>
2025-09-08 13:52:15 +0200

    update done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './contacts/list/list.component';
+import { AddComponent } from './contacts/add/add.component';
+import { ViewComponent } from './contacts/view/view.component';
+import { UpdateComponent } from './contacts/update/update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contacts', component: ListComponent },
+  { path: 'contacts/add', component: AddComponent },
+  { path: 'contacts/:id', component: ViewComponent },
+  { path: 'contacts/:id/update', component: UpdateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
